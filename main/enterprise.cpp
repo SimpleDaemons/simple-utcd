fde2ba98d4d48b36a9b9f51e7d522fe2e1ac9a35
Brandon L. Burns <brandon@blburns.com>
2026-01-21 20:25:08 -0600

    feat: Add enterprise and datacenter main entry points

---
+++ main/enterprise.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file enterprise.cpp
+ * @brief Main entry point for Simple UTC Daemon Enterprise Version
+ * @author SimpleDaemons
+ * @copyright 2024 SimpleDaemons
+ * @license BSL 1.1
+ */
+
+int main(int argc, char* argv[]) {
+    // Enterprise version implementation coming soon
+    return 0;
+}
